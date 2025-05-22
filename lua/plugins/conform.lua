9fd8ef52ac50ec799a21754dbadddb66afc8c1d6
Léo <leonardooliveira.sl@gmail.com>
2025-05-22 00:40:53 -0300

    fix: Adjusts conform and adds new theme poimandres

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,23 +1,11 @@
 return {
   "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    require("conform").setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-        javascript = { "eslint_d" },
-        typescript = { "eslint_d" },
-        typescriptreact = { "eslint_d" },
-        javascriptreact = { "eslint_d" },
-        json = { "prettierd", "prettier" },
-        html = { "prettierd", "prettier" },
-        css = { "prettierd", "prettier" },
-      },
-      formatters = {
-        eslint_d = {
-          prepend_args = { "--fix-dry-run", "--format", "json" },
-        },
-      },
-    })
-  end,
+  opts = {
+    formatters_by_ft = {
+      javascript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescript = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+    },
+  },
 }
